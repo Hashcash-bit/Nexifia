7fcd2159caf113b23c8f9da85b5371a5d84384e9
Hashcash-bit <hashirimam2004@gmail.com>
2023-05-13 22:03:48 -0400

    Login Page start

--- Nexifia/ExternalScreens/Loginpage.js
+++ Nexifia/ExternalScreens/Loginpage.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import {Text, Image} from "react-native"
+//Important React dependencies
+import React from "react";
+import { Text, Image, View } from "react-native";
 
-const Loginpage = () => {
+//Firebase Dependencies
+
+const Loginpage = ({ navigation }) => {
   return (
-    <Text>Loginpage</Text>
-  )
-}
+    <View
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#131417",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          height: 100,
+          // backgroundColor: 'yellow',
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={require("../assets/NEXIFIA.png")}
+          style={{
+            width: 217,
+            height: 40,
+            marginTop: 50,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 0,
+        }}
+      >
+        <Image
+          source={require("../assets/phonelogin.png")}
+          style={{
+            width: 259,
+            height: 530,
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        />
+      </View>
+    </View>
+  );
+};
 
-export default Loginpage
+export default Loginpage;
