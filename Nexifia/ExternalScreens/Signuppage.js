93adb7d5a00d01e130ca16c3b4664910544d5855
Hashcash-bit <hashirimam2004@gmail.com>
2023-05-25 01:03:34 -0400

    Error Catching done for the Signup Page

--- Nexifia/ExternalScreens/Signuppage.js
+++ Nexifia/ExternalScreens/Signuppage.js
@@ -8,6 +8,7 @@ import {
   TextInput,
   ImageBackground,
   KeyboardAvoidingView,
+  ToastAndroid,
 } from "react-native";
 
 //Database Dependencies
@@ -19,6 +20,12 @@ export const Signuppage = ({ navigation }) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState(""); // Confirm State
 
+  // Some regex error catching const method (pretty cool)--
+  const lowercaseRegex = /[a-z]/; //                      |
+  const uppercaseRegex = /[A-Z]/; //                      |
+  const numberRegex = /[0-9]/; //                         |
+  // ------------------------------------------------------
+
   //Time to do some error handling
   const [showErrors, setShowErrors] = useState(false);
   const [errors, setErrors] = useState({});
@@ -28,27 +35,74 @@ export const Signuppage = ({ navigation }) => {
     // This is the error handling for the email
     if (!email) {
       // If there is no email value in the text input display the message below
-      errors.email = "Please Enter Your Email";
+      errors.email = "Please enter your email";
     } else if (!email.includes("@") || !email.includes(".com")) {
       // If there is no @ sign and .com in the email display the message below
-      errors.email = "Please Enter a Proper Email (johndoe@whatever.com)";
+      errors.email = "Please enter a proper email (johndoe@whatever.com)";
     }
 
     //This will be the error handling for the confirmEmail
+    if (confirmEmail != email) {
+      // The two emails entered must be the same or else the message below is shown
+      errors.confirmEmail = "Please make sure that your emails match ";
+    }
+
     //This will be the error handling for the Password
+    if (!password) {
+      // If there is no password in the text field display the message below
+      errors.password = "Please enter your password";
+    } else if (password.length < 8) {
+      // If the password is less than 8 characters long display the message below
+      errors.password = "The password must be at least 8 characters long";
+    } else if (
+      !password.includes("!") &&
+      !password.includes("@") &&
+      !password.includes("#") &&
+      !password.includes("$") &&
+      !password.includes("%") &&
+      !password.includes("^") &&
+      !password.includes("&")
+    ) {
+      // If the password doesnt include any of the following characters display the message below
+      errors.password =
+        "Your password must include any of these following symbols (!,@,#,$,%,^,&)";
+    } else if (password === email) {
+      // If password and email are the same display the message below
+      errors.password =
+        "Please make sure that your email and passwords are not the same";
+    } else if (!lowercaseRegex.test(password)) {
+      errors.password = "Please include at least 1 small letter";
+    } else if (!uppercaseRegex.test(password)) {
+      errors.password = "Please include at least 1 capital letter";
+    } else if (!numberRegex.test(password)) {
+      errors.password = "Please include at least 1 number";
+    }
+
     //This will be the error handling for the confirmPassword
+    if (confirmPassword != password) {
+      errors.confirmPassword = "Please make sure that your passwords match";
+    }
+
     return errors;
   };
 
   // To visualize the onPress action of the button
   const handleRegister = () => {
-    const errors = getErrors(email, password, confirmPassword);
+    const errors = getErrors(email, confirmEmail, password, confirmPassword);
     if (Object.keys(errors).length > 0) {
       setShowErrors(true);
       setErrors(showErrors && errors);
-      console.log(errors);
+      console.log(errors, email, password);
+
+      Object.values(errors).forEach((errorMsg) => {
+        ToastAndroid.show(errorMsg, ToastAndroid.SHORT);
+      });
+    } else {
+      console.log("Registered");
+      setErrors({});
+      showErrors(false);
+      ToastAndroid.show("Registered", ToastAndroid.TOP, ToastAndroid.LONG);
     }
-    console.log("Registered");
   };
 
   return (
@@ -115,6 +169,7 @@ export const Signuppage = ({ navigation }) => {
               borderRadius: 15,
             }}
           />
+
           <TextInput
             placeholder="Confirm Your Email Address"
             placeholderTextColor={"#7D7E80"}
