a6e87568376c5f10e2e68da010a67b05b38a1533
Hashcash-bit <hashirimam2004@gmail.com>
2023-06-08 02:27:47 -0400

    Experience Done

--- Nexifia/InternalScreens/HomeScreen.js
+++ Nexifia/InternalScreens/HomeScreen.js
@@ -12,17 +12,24 @@ import {
 
 // firebase Imports
 import { firebase_auth } from "../firebase";
+import { TextInput } from "react-native-gesture-handler";
 
 const HomeScreen = ({ navigation }) => {
-
   return (
+    // I am testing the firestore to the see how it works
+    // Make a quick input text field and see if it works
     <View
       style={{
         flex: 1,
         backgroundColor: "#131417",
         fontColor: "white",
       }}
-    ></View>
+    >
+      <TextInput  />
+      <TouchableOpacity onPress>
+        <Text>Test</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
